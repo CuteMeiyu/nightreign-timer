3cfafdd7ec53f1723b997abab5a52e98d78577e8
CuteMeiyu <cutemeiyu@163.com>
2025-06-22 10:43:12 +0800

    add threshold for each target detection

--- splits/ocr.py
+++ splits/ocr.py
@@ -6,17 +6,18 @@ TARGETS = [
     {
         "rect": (0.4152, 0.4979, 0.5832, 0.5861),
         "texts": ["DAY I", "DAY II"],
+        "threshold": 50,
     },
     {
         "rect": (0.7879, 0.8396, 0.9305, 0.8694),
         "texts": ["已得到恐怖强敌的所在地信息"],
+        "threshold": 70,
     },
 ]
 DEBUG = False  # Save temp images
 MONITOR = 1  # Monitor index, starting with 1. https://python-mss.readthedocs.io/api.html#mss.tools.mss.base.MSSBase.monitors
 INTERVAL = 0.5  # Interval between twice detection
 COOLDOWN = 30  # Cooldown after split
-THRESHOLD = 50  # Text similarity threshold
 
 
 async def start():
@@ -50,7 +51,7 @@ async def start():
                 if DEBUG:
                     Image.fromarray(img).save(f"temp-{i}.png")
                 recognized = ocr(img)
-                if any(fuzz.ratio(recognized, text) >= THRESHOLD for text in target["texts"]):
+                if any(fuzz.ratio(recognized, text) >= target["threshold"] for text in target["texts"]):
                     print(recognized)
                     server.send_action("split")
                     await asyncio.sleep(COOLDOWN)
